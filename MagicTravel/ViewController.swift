b042e979a100e168d8b9bdf66cf1f7abf8481b2f
ivan <itelles@mahalobanking.com>
2023-06-23 16:00:08 -0700

    Initial Commit

---
+++ MagicTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTravel
+//
+//  Created by Telles Ivan on 6/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
